3d3502bd76ac726014e19f0d9bc2425c3cc59e36
kenn1d <makarkibanov220@gmail.com>
2026-03-02 11:39:58 +0500

    Доработка JS скриптов для уменьшения и увеличения предметов в корзине

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -32,6 +32,15 @@ namespace praktika22.Controllers
             return View(VMItems);
         }
 
+        public ActionResult Basket(int idItem = -1)
+        {
+            if(idItem != -1)
+            {
+                Startup.BasketItem.Add(new ItemsBasket(1, IAllItems.AllItems.Where(x => x.Id == idItem).First()));
+            }
+            return Json(Startup.BasketItem);
+        }
+
         [HttpGet]
         public ViewResult Add()
         {
